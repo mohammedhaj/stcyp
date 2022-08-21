6bc517e8a0dfcd141d1aadda293bfaca17caf7e0
hajjej <medhajjej16@gmail.com>
2022-08-21 16:06:53 +0200

    cyp light rep

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,9 +3,8 @@ module.exports = {
       // Static site example
 collect: {
     numberOfRuns: 1,
-   // staticDistDir: "./public --url:['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']"    
-    
-   url: ['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']
+    //staticDistDir: "./cypress/e2e",
+    url: ['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']
   },
   upload: {
     target: 'temporary-public-storage',
