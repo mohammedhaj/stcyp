b2b4b07d0ed5e24f7ad5f792f51e5d71fad588c8
hajjej <medhajjej16@gmail.com>
2022-08-21 10:49:21 +0200

    add light

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ module.exports = {
       // Static site example
 collect: {
     staticDistDir: './cypress/e2e',
-   // url: ['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']
+     url: ['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']
   },
   upload: {
     target: 'temporary-public-storage',
