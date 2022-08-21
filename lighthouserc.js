79142aa22c12fa70243127865bec580da7d265ee
hajjej <medhajjej16@gmail.com>
2022-08-21 15:58:06 +0200

    brach dtaticDir

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,8 +3,10 @@ module.exports = {
       // Static site example
 collect: {
     numberOfRuns: 1,
+    staticDistDir: "./public --url=http://localhost/checkout.html"
     //staticDistDir: "./cypress/e2e",
-    url: ['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']
+    
+   // url: ['https://jpetstore.tricentis.com/jpetstore/actions/Catalog.action']
   },
   upload: {
     target: 'temporary-public-storage',
